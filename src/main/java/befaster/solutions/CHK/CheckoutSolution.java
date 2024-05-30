43a8c668abcf726ca454ac0d052207595a3f0a72
sbx_user1051 <sbx_user1051@169.254.42.253>
2024-05-30 23:33:44 +0000

    Thu May 30 23:33:44 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        if ( !skus.matches("[ABCDEF]*")){
+        if ( !skus.matches("[A-Z]*")){
             return -1;
         }
         Map<Character, Integer> itemCounts= new HashMap<>();
@@ -37,7 +37,86 @@ public class CheckoutSolution {
             total += 20;
         }else { total += remainingF *10;
         }
+        int countG =itemCounts.getOrDefault('G', 0);
+        total += countG * 20;
+
+        int countH =itemCounts.getOrDefault('H', 0);
+        total += (countH/10) *80 + (( countH % 10) /5 )*45 + (countH % 10 % 5) * 10;
+
+        int countI =itemCounts.getOrDefault('I', 0);
+        total += countI *35;
+
+        int countJ =itemCounts.getOrDefault('J', 0);
+        total += countJ * 60;
+
+        int countK =itemCounts.getOrDefault('K', 0);
+        total += (countK / 2) *150 + (countK %2)*80;
+
+        int countL =itemCounts.getOrDefault('L', 0);
+        total += countL *90;
+
+        int countM =itemCounts.getOrDefault('M', 0);
+
+        int countN =itemCounts.getOrDefault('N', 0);
+        total += countN *40;
+
+        int freeM =countN /3;
+        countM -= freeM;
+
+        int countO =itemCounts.getOrDefault('O', 0);
+        total += countO *10;
+
+        int countP =itemCounts.getOrDefault('P', 0);
+        total += (countP / 5) * 200 + (countP % 5) *50;
+
+        int countQ =itemCounts.getOrDefault('Q', 0);
+       // total += (countQ / 3) * 80 + (countP % 3) *30;
+
+        int countR =itemCounts.getOrDefault('R', 0);
+        total += countR * 50 ;
+        int freeQ = countR /3;
+        countQ -= freeQ;
+
+        int countS =itemCounts.getOrDefault('S', 0);
+        total += countS * 30 ;
+
+        int countT =itemCounts.getOrDefault('T', 0);
+        total += countT * 20 ;
+
+        int countU =itemCounts.getOrDefault('U', 0);
+        int setsofFour = countU/ 4;
+        int remainingU = countF % 4;
+        total += setsofFour * 120;
+        if (remainingU == 3) {
+            total += 120;
+        }else { total += remainingU *40;
+        }
+
+        int countV =itemCounts.getOrDefault('V', 0);
+        total += (countV /3) * 130 + ((countV %3)/2) * 90 + (countV % 3 % 2) * 50;
+
+        int countW =itemCounts.getOrDefault('W', 0);
+        total += countW * 20 ;
+
+        int countX =itemCounts.getOrDefault('X', 0);
+        total += countX * 90 ;
+
+        int countY =itemCounts.getOrDefault('Y', 0);
+        total += countY * 10 ;
+
+        int countZ =itemCounts.getOrDefault('Z', 0);
+        total += countZ * 50 ;
+
+        if(countM >0 ){
+            total += countM * 15;}
+
+        if(countQ>0){
+            total+= (countQ / 3) * 80 + (countQ % 3) * 30;
+        }
+
+
 
         return total;
     }
 }
+
